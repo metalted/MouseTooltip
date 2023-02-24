5b32503f654a3eed5eddbe70e18b8618eab8e1f6
Metalted <68029407+metalted@users.noreply.github.com>
2023-02-24 19:17:54 +0100

    Initial commit

---
+++ Plugin.cs
@@ -0,0 +1,22 @@
+﻿using BepInEx;
+using HarmonyLib;
+using UnityEngine;
+using System.Collections;
+
+namespace Cardio
+{
+    [BepInPlugin(pluginGuid, pluginName, pluginVersion)]
+    public class BaseZeepKistPlugin : BaseUnityPlugin
+    {
+        public const string pluginGuid = "com.metalted.zeepkist.basezeepkistplugin";
+        public const string pluginName = "BaseZeepkistPlugin";
+        public const string pluginVersion = "1.0";
+
+        public void Awake()
+        {
+            Harmony harmony = new Harmony(pluginGuid);
+            harmony.PatchAll();
+            Logger.LogInfo($"Plugin {pluginName} is loaded!");
+        }
+    }
+}
